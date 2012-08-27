7b709466478509700dc97b50b2702138b7380866
stanley <stanley.w.cai@gmail.com>
2012-08-27 22:47:10 +0800

    Transfer to Redis backend
    Signed-off-by: stanley <stanley.w.cai@gmail.com>

--- blog.go
+++ blog.go
@@ -1,76 +1,149 @@
-//package blog
 package main
 
 import (
-	"os"
+//	"os"
 	"log"
 	"html/template"
-	"path"
+//	"path"
 	"net/http"
-	"io/ioutil"
-	)
+//	"io/ioutil"
+	"encoding/json"
+	"time"
+//	"sort"
+	"strconv"
+//	"bytes"
+	"github.com/simonz05/godis"
+)
 
-type Blog struct {
+
+type BlogEntry struct {
 	Title	string
 	Body	[]byte
+	Date	int64
 }
+
+type BlogStruct struct {
+	Name	string
+	Author	string
+	Email	string
+	Entries	[]BlogEntry
+}
+
+var (
+	client	*godis.Client
+	blog	BlogStruct = BlogStruct{Name:"Stanley's Blog", Author:"stanley", Email:"stanley.w.cai@gmail.com"}
+)
 
 const (
-	blogPrefix = "f"
-	lenPath = len("/view/")
-	)
+	blogPrefix = "/blog/view/"
+)
 
-func (b *Blog) save() error {
-	filename := path.Join(blogPrefix, b.Title + ".txt")
-	return ioutil.WriteFile(filename, b.Body, 0600)
-}
-
-func load(title string) (*Blog, error) {
-	filename := path.Join(blogPrefix, title + ".txt")
-	body, err := ioutil.ReadFile(filename)
+func (b *BlogStruct) add(entry BlogEntry) {
+	log.Println("entry %v", entry)
+	key := strconv.FormatInt(time.Now().Unix(), 10)
+	value, err := json.Marshal(entry)
+	log.Printf("entry %v value %v\n", entry, value)
 	if err != nil {
-		return nil, err
+		// log error msg and return
+		log.Printf("failed to marshal(entry %v)\n", entry)
+		return
 	}
-	return &Blog{Title: title, Body: body}, nil
+
+	client.Hset("blog", key, value)
+
+	// TODO This implementation is dirty. Optimize this.
+	// reload all blog entries
+	b.loadAll()
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func addNewEntries() {
+	client.Flushdb()
+
+	entry := BlogEntry{"hello, world", []byte("hello, go world"), time.Now().Unix()}
+	blog.add(entry)
+	entry = BlogEntry{"hello again", []byte("hello, GO world"), time.Now().Unix()}
+	blog.add(entry)
+	log.Println(blog)
+}
+
+func (b *BlogStruct) loadAll() {
+	actual, err := client.Hkeys("blog")
+	if err != nil {
+		// log error msg and return
+		log.Printf("failed to invoke Hkeys(blog) %v\n", actual)
+		return
+	}
+
+	keys := make([]int64, len(actual))
+	for i, key := range actual {
+		keys[i], err = strconv.ParseInt(key, 10, 0)
+		if err != nil {
+			// log err message and continue
+			log.Printf("failed to convert key(%v) into int64\n", key)
+			keys[i] = 0
+		}
+	}
+
+	// TODO: need sort the keys at first
+	b.Entries = make([]BlogEntry, len(keys))
+	for i, strKey := range actual {
+		// JSON string to blogStruct
+		value, err := client.Hget("blog", strKey)
+		if err != nil {
+			// log error message and continue
+			log.Printf("failed to hget(blog, %v)\n", strKey)
+			continue
+		}
+		err = json.Unmarshal(value.Bytes(), &b.Entries[i])
+		if err != nil {
+			// log error message and continue
+			log.Printf("failed to unmarshal JSON %v\n", value)
+		}
+		log.Printf("value %v entry %v", value, b.Entries[i])
+	}
+	// Done
+}
+
+func blogIndex(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if _, ok := recover().(error); ok {
-			log.Println("Failed to init blog")
+			// go error.html
+			log.Println("failed")
 		}
 	}()
-	d, err := os.Open(blogPrefix)
-	if err != nil {
-		panic(err)
-	}
-
-	files, err := d.Readdir(-1)
-	if err != nil {
-		panic(err)
-	}
-
-	list := make([]Blog, 0)
-	for i := range files {
-		if !files[i].IsDir() {
-			name := files[i].Name()
-			b := Blog{name[:len(name)-len(path.Ext(name))], nil}
-			list = append(list, b)
-		}
+	if blog.Entries == nil {
+		blog.loadAll()
 	}
 	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, list)
+	t.Execute(w, blog)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[lenPath:]
-	p, _ := load(title)
+	id := r.URL.Path[len(blogPrefix):]
+	if blog.Entries == nil {
+		blog.loadAll()
+	}
+
+	p, err := strconv.ParseInt(id, 10, 0)
+	if err != nil {
+		p = 0
+	}
+	var entry BlogEntry
+	for _, e := range blog.Entries {
+		if e.Date == p {
+			entry = e
+			break
+		}
+	}
+
 	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, p)
+	t.Execute(w, entry)
 }
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/view/", viewHandler)
+	client = godis.New("", 0, "")
+	addNewEntries()
+	http.HandleFunc("/blog/", blogIndex)
+	http.HandleFunc("/blog/view/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
